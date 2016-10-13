923e65f277f28236990ef40c9c643bb9dd54e0ea
KANHAIYA <kanhaiya.1609@gmail.com>
2016-10-13 14:26:16 +0530

    Convert application layout to haml and added header also some changement in Index file

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ApplicationRecord
 
 	has_many :comments
 
-	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
+	has_attached_file :image, styles: { medium: "700x500#", small: "350x250#" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
